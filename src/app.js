584b257b2db2095bbd92ccfe1f5a51d5ba324e6c
JonaPortillo <portillo.jonathan.leonel@gmail.com>
2022-04-16 02:52:26 -0300

    Creando nuestras propias APIs - Desafío 1 - Controlador y ruta para api/genres, y api/genres/detail/id

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
+const app = express();
 
 const indexRouter = require('./routes/index');
 
 const moviesRoutes = require('./routes/moviesRoutes');
 const genresRoutes = require('./routes/genresRoutes');
-const app = express();
+const apiMoviesRoutes = require('./routes/api/moviesRoutes');
+const apiGenresRoutes = require('./routes/api/genresRoutes');
 
 // view engine setup
 app.set('views', path.resolve(__dirname, './views'));
@@ -25,6 +27,8 @@ app.use('/', indexRouter);
 app.use(moviesRoutes);
 app.use(genresRoutes);
 
-
+/* RUTAS API */
+//app.use('api/movies', apiMoviesRoutes);
+app.use('/api/genres', apiGenresRoutes)
 
 app.listen('3001', () => console.log('Servidor corriendo en el puerto 3001, http://localhost:3001'));
